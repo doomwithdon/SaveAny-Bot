eca5ef740a4140310bb19e611f759e1a2ecb0954
doomwithdon <doomwithdon@yahoo.com>
2025-06-15 22:03:27 -0400

    Update middleware.go

--- client/bot/handlers/middleware.go
+++ client/bot/handlers/middleware.go
@@ -12,10 +12,10 @@ import (
 func checkPermission(ctx *ext.Context, update *ext.Update) error {
 	userID := update.GetUserChat().GetID()
 	if !slice.Contain(config.Cfg.GetUsersID(), userID) {
-		const noPermissionText string = `
-您不在白名单中, 无法使用此 Bot.
+		const noPermissionText string = ``
+/*您不在白名单中, 无法使用此 Bot.
 您可以部署自己的实例: https://github.com/krau/SaveAny-Bot
-`
+`*/
 		ctx.Reply(update, ext.ReplyTextString(noPermissionText), nil)
 		return dispatcher.EndGroups
 	}
